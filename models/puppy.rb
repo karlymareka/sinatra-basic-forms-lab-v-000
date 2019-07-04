8018531f3a340cd1f3c2e427296a2b1277807bd7
Karly Mareka <karly.mareka@gmail.com>
2019-07-04 23:23:31 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -4,7 +4,9 @@ class Puppy
   attr_reader :name, :breed 
   
   def initialize(name, breed, months_old)
-    
+    @name = name 
+    @breed = breed 
+    @months_old = months_old 
   end 
   
 end 
